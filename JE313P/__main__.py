c49ea068a796bbd4ba4e5e6feaa7fbc698bf20b4
LordSan9 <104519846+LordSan9@users.noreply.github.com>
2022-07-04 21:06:58 +0300

    Update __main__.py

--- JE313P/__main__.py
+++ JE313P/__main__.py
@@ -19,7 +19,7 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
     
 print("تم تنصيب السورس بنجاح")
-print("قناة السورس @JMTHON")
+print("قناة السورس @IIlAndylII")
 
 if __name__ == "__main__":
     JE313P.run_until_disconnected()
